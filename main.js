89f992e55e29ccfd8174237d1907ebbeb690cb4d
alirangkul <ali.nurcahya@barangkulakan.com>
2024-02-29 19:00:27 +0700

    initial project

---
+++ main.js
@@ -0,0 +1,91 @@
+// 1. Menghitung 5 angka
+let angka1 = 10 + 10 + 10 + 10 + 10;
+console.log(angka1);
+
+let angka2 = 10 * 10 / (10 ** 10) - 10;
+console.log(angka2);
+
+let angka3 = (5 * 5) - 5 + 5 - 5 + 5;
+console.log(angka3);
+
+let angka4 = (20 / 4) + 2 - 5 + 3 + 1;
+console.log(angka4);
+
+let angka5 = (25 % 6) + 3 - 1 + 4 - 2;
+console.log(angka5);
+
+let angka6 = 15 - 3 + 2 * 5 - 4;
+console.log(angka6);
+
+let angka7 = (8 ** 2) + 10 - 3 + 4 - 7;
+console.log(angka7);
+
+let angka8 = (50 / 5) + 6 - 3 + 2 * 2;
+console.log(angka8);
+
+let angka9 = 30 + 7 - 9 * 2 + 5 - 3;
+console.log(angka9);
+
+let angka10 = (100 - 50) * 2 + 10 - 5 + 3;
+console.log(angka10);
+
+let angka11 = 5 + 5 + 5 + 5 + 5;
+console.log(angka11);
+
+let angka12 = 25 - 10 + 3 * 2 + 6 - 4;
+console.log(angka12);
+
+let angka13 = (40 / 4) + 5 - 2 * 3 + 7 - 6;
+console.log(angka13);
+
+let angka14 = (18 ** 2) + 15 - 4 * 2 + 10 - 6;
+console.log(angka14);
+
+let angka15 = (60 - 30) * 2 + 8 - 5 * 2 + 6;
+console.log(angka15);
+
+let angka16 = 45 + 5 - 4 * 3 + 6 - 2;
+console.log(angka16);
+
+let angka17 = 20 - 5 * 2 + 3 - 1 + 4;
+console.log(angka17);
+
+let angka18 = (90 / 9) + 7 - 3 + 4 * 2 - 5;
+console.log(angka18);
+
+let angka19 = (120 - 60) * 2 + 10 - 4 + 5;
+console.log(angka19);
+
+let angka20 = 35 + 8 - 6 * 3 + 4 - 1;
+console.log(angka20);
+
+// 2. Operator Perbandingan
+let perbandingan1 = 10 > 5;
+console.log(perbandingan1);
+
+let perbandingan2 = 10 < 20;
+console.log(perbandingan2);
+
+let perbandingan3 = 15 >= 15;
+console.log(perbandingan3);
+
+let perbandingan4 = 20 <= 18;
+console.log(perbandingan4);
+
+let perbandingan5 = 25 === 25;
+console.log(perbandingan5);
+
+let perbandingan6 = 30 !== 30;
+console.log(perbandingan6);
+
+let perbandingan7 = 8 === 8;
+console.log(perbandingan7);
+
+let perbandingan8 = 12 !== 10;
+console.log(perbandingan8);
+
+let perbandingan9 = 20 >= 15;
+console.log(perbandingan9);
+
+let perbandingan10 = 25 <= 30;
+console.log(perbandingan10);
